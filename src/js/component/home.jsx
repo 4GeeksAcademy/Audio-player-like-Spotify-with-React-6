145067999e6d8694e04ecc1c0390a04623cb0461
rociomr08 <161978284+rociomr08@users.noreply.github.com>
2024-08-16 22:36:07 +0000

    implementacion de play

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,22 +2,44 @@ import React, { useState, useEffect, useRef } from "react";
 
 //create your first component
 const Home = () => {
-	const [songs, setSongs] = useState([])
+	const [songs, setListSongs] = useState([])
+	const [urlSong, setUrlSong] = useState("")
+	const [currentSongId, setCurrentSongId] = useState("")
 
+
+	//Obtener la lista de canciones
 	const getSongs = () => {
 		fetch("https://playground.4geeks.com/sound/songs")
 			.then((response) => response.json())
 			.then((data) => {
-				setSongs(data.songs)
+				setListSongs(data.songs)
 			})
 
 	}
+	//Declaramos el espacio de memoria donde usamos el useRef
+	const audioElement = useRef();
+	console.log(audioElement);
+
+	// procesa la url de la cancion traida con el onclick
+	//se activa un FOCO en la etiqueta audio accesada previamente con useRef
+	const playSong = (url) => {
+		audioElement.current.src = "https://playground.4geeks.com" + url;
+		audioElement.current.play();
+		console.log(url);
+	}
+
+	//Funcion para reproducir la cancion anterior usando id
+	const prevSong = (id) => {
 
 
+	}
 
+	//Funcion para reproducir la siguiente cancion usando id
+	const nextSong = () => {
 
+	}
 
-
+	//Hacer que aparezcan las lista de canciones obtenidas desde la API  al cargar el componente en el navegador
 	useEffect(() => {
 		getSongs()
 	}, [])
@@ -25,22 +47,29 @@ const Home = () => {
 	return (
 		<div className="text-center">
 			<h3 className="text-white">Lista de canciones </h3>
-			<ol>
+			<ol className="list-group list-group-numbered bg-dark">
 				{
 					songs.map((item, index) => {
 						return (
 							<div key={index} >
-								<li className="text-white" key={item.name} onClick={}>{item.name}</li>
+								{/* Al hacer click en la cancion se captura la url de la cancion y se ejecuta la funcion PlaySOng  console.log(item.url) */}
+								<li className="text-white list-group-item bg-dark" key={item.name} onClick={() => {
+									playSong(item.url),
+										setCurrentSongId(item.id);
+								}}>{item.name} </li>
 							</div>
 						)
 					})
 				}
 			</ol>
-			<audio>
-				<source src="horse.ogg" type="audio/ogg"/>
-				<source src="horse.mp3" type="audio/mpeg" />
+			<audio ref={audioElement}>
+
 			</audio>
-			<button>Play</button>
+
+			<button onClick={() => prev(urlSong)}><i className="fa fa-backward"></i></button>
+			<button onClick={() => playSong(urlSong)}><i className="fa fa-play"></i></button>
+			<button onClick={() => nextSong(urlSong)}><i className="fa fa-forward"></i></button>
+
 
 		</div>
 	);
