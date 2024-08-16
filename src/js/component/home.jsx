fb3b49428828668185ab19378ee0952e5a728e17
rociomr08 <161978284+rociomr08@users.noreply.github.com>
2024-08-16 16:56:40 +0000

    obteniendo la music de la api

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,47 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect, useRef } from "react";
 
 //create your first component
 const Home = () => {
+	const [songs, setSongs] = useState([])
+
+	const getSongs = () => {
+		fetch("https://playground.4geeks.com/sound/songs")
+			.then((response) => response.json())
+			.then((data) => {
+				setSongs(data.songs)
+			})
+
+	}
+
+
+
+
+
+
+	useEffect(() => {
+		getSongs()
+	}, [])
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h3 className="text-white">Lista de canciones </h3>
+			<ol>
+				{
+					songs.map((item, index) => {
+						return (
+							<div key={index} >
+								<li className="text-white" key={item.name} onClick={}>{item.name}</li>
+							</div>
+						)
+					})
+				}
+			</ol>
+			<audio>
+				<source src="horse.ogg" type="audio/ogg"/>
+				<source src="horse.mp3" type="audio/mpeg" />
+			</audio>
+			<button>Play</button>
+
 		</div>
 	);
 };
